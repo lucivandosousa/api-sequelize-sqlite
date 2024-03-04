f5b683e4698a9f6b5bef8f7a799ddf16e2ba2100
Lucivando Sousa <lucivandosousa@gmail.com>
2024-03-04 15:13:27 -0300

    feat: criar documentacao com swagger

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require("express")
 const { productsRoute } = require("./routes")
+const swaggerUi = require("swagger-ui-express")
+const swaggerDocument = require("./swagger.json")
 
 const app = express()
 
 app.use(express.json())
 app.use(productsRoute)
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.get("/", (req, res) => res.send({"message": "API com Sequelize e SQLite"}))
 
